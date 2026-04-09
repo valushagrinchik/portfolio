eda6dda4f1eca71d19a7bb760e38b7ab5ab26d73
valushagrinchik <valushagrinchik@gmail.com>
2026-04-09 13:01:36 +0300

    first version

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Привет 👋</h1>
+      <h2>Я Full Stack разработчик</h2>
+      <p>Делаю веб-приложения и чат-ботов</p>
+    </div>
+  )
+}
+
+export default App
